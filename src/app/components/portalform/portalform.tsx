140489d16c5a6367c2104fc81d247149fb456d3a
Jai Pannu <142983705+JaiPannu-IITI@users.noreply.github.com>
2025-01-29 00:00:38 +0530

    changes

--- src/app/components/portalform/portalform.tsx
+++ src/app/components/portalform/portalform.tsx
@@ -9,25 +9,12 @@ export default function PortalForm() {
   const words = `Training and Placement Cell IIT Indore, is the organization which handles all activities related to the campus placement of the graduating batch.
 `;
   return (
-    <div className=" w-[45%] relative z-10  ">
-      {/* <div className=" flex items-center">
-        <div className=" bg-white pl-4 pr-[1.1rem] pt-5 pb-5 rounded-[50%] flex min-w-[147px] h-[140px] items-center justify-center">
-          <Image
-            width={400}
-            height={400}
-            src={"/data/logo/iitiold.png"}
-            alt="iit indore"
-            className="w-[100px] h-[100px] relative z-10"
-          />
-        </div>
-        <div className=" w-[100vw] h-[5px] absolute left-0 bg-white "></div>
-      </div> */}
-      <div className=" text-[4rem] uppercase text-white font-bold">
+    <div className=" w-[45%] relative z-10 h-full flex flex-col items-start justify-center  ">
+      
+      <div className=" text-[2.5rem] lg:text-[3rem] xl:text-[3.5rem] 2xl:text-[4rem] uppercase text-white font-bold">
         Training And Placement Cell
       </div>
-      {/* <div className=" text-[3rem] uppercase text-white font-thin ">
-        IIT Indore
-      </div> */}
+     
       <div className=" text-white opacity-70 font-thin">
         <TextGenerateEffect words={words} />
       </div>
