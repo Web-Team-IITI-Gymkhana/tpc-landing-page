9d60493f5c52805c6ecb0447c8320170048fa8d5
JaiPannu-IITI <pannujai06iiti@gmail.com>
2025-02-25 13:27:15 +0530

    admin

---
+++ src/app/admin/dashboard/components/job/components/data.js
@@ -0,0 +1,47 @@
+import React from "react";
+const columns = [
+  { name: "JOB", uid: "job" },
+  { name: "LAST DATE", uid: "lastDate" },
+  { name: "STATUS", uid: "status" },
+  { name: "ACTIONS", uid: "actions" },
+];
+
+const users = [
+  {
+    id: 1,
+    job: "Research Intern at Microsoft",
+    lastDate: "2025-09-01",
+    status: "active",
+    link: "https://jobs.careers.microsoft.com/global/en/job/1582723/Research-Intern?utm_medium=Copy-job-share",
+  },
+  {
+    id: 2,
+    job: "Research Intern at Microsoft",
+    lastDate: "2025-09-01",
+    status: "active",
+    link: "https://jobs.careers.microsoft.com/global/en/job/1582723/Research-Intern?utm_medium=Copy-job-share",
+  },
+  {
+    id: 3,
+    job: "Research Intern at Microsoft",
+    lastDate: "2025-09-01",
+    status: "active",
+    link: "https://jobs.careers.microsoft.com/global/en/job/1582723/Research-Intern?utm_medium=Copy-job-share",
+  },
+  {
+    id: 4,
+    job: "Research Intern at Microsoft",
+    lastDate: "2025-09-01",
+    status: "active",
+    link: "https://jobs.careers.microsoft.com/global/en/job/1582723/Research-Intern?utm_medium=Copy-job-share",
+  },
+  {
+    id: 5,
+    job: "Research Intern at Microsoft",
+    lastDate: "2025-09-01",
+    status: "active",
+    link: "https://jobs.careers.microsoft.com/global/en/job/1582723/Research-Intern?utm_medium=Copy-job-share",
+  },
+];
+
+export { columns, users };
