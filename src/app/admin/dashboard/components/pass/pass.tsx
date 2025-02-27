432c5d2c2c544b0f2a83a781e78e21b65470b2e1
JaiPannu-IITI <pannujai06iiti@gmail.com>
2025-02-27 17:35:31 +0530

    Revert "remove admin"
    This reverts commit 5379830278e25826947275bf827e5ed6be6f1f37.

---
+++ src/app/admin/dashboard/components/pass/pass.tsx
@@ -0,0 +1,7 @@
+export default function Pass() {
+  return (
+    <>
+      <h1>pass</h1>
+    </>
+  );
+}
---
+++ src/app/admin/dashboard/components/team/team.tsx
@@ -0,0 +1,7 @@
+export default function Team() {
+  return (
+    <>
+      <h1>team</h1>
+    </>
+  );
+}
